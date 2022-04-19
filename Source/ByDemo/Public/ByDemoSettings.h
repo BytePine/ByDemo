7e7d0a9c0613204051ccea8c07a493918929e2f2
byteyang <byteyang@tencent.com>
2022-04-19 21:17:59 +0800

    添加基础类

---
+++ Source/ByDemo/Public/ByDemoSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "ByDemoSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(config = Game, meta = (DisplayName = "ByDemo"))
+class BYDEMO_API UByDemoSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ByDemo/Public/ByGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ByGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BYDEMO_API UByGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
